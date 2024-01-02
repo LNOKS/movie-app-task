2545e13f8d7ae7bd88692fa37fa06a90044fa3d8
Oleh Ferents <oleh.ferents@lnoks.com>
2024-01-02 13:26:03 +0200

    Fix env variables

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
         domains: ['cdn.builder.io'],
     },
+    env: {
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        DATABASE_URL: process.env.DATABASE_URL,
+    }
 }
 
 module.exports = nextConfig
